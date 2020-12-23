bbfd13f481d5f6946ece25ad591c0ad9a8b8b915
Sergey Bezsonov <mr.nosomebody@gmail.com>
2020-12-23 18:35:49 +1000

    pognali nahoi

---
+++ marathon/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Person)
+admin.site.register(Sponsor)
+admin.site.register(Status)
+admin.site.register(Distance)
+admin.site.register(Event)
+admin.site.register(Run)
---
+++ marathon/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Distance(models.Model):
+    distance = models.DecimalField(max_digits=4,decimal_places=2)
+
+class Event(models.Model):
+    name = models.CharField(max_length=40)
+    date = models.DateTimeField(default=timezone.now)
+    place = models.CharField(max_length=255)
+    distance_id = models.ForeignKey(Distance, on_delete=models.PROTECT)
+
+class Sponsor(models.Model):
+    company = models.CharField(max_length=100 )
+    phone_num = models.CharField(max_length=12)
+    e_mail = models.CharField(max_length=50)
+    event_id = models.ManyToManyField(Event)
+
+class Status(models.Model):
+    name = models.CharField(max_length=40)
+
+class Person(User):
+    third_name = models.CharField(max_length=40)
+    sex = models.CharField(max_length=10)
+    phone_num = models.CharField(max_length=12)
+    status_id = models.ManyToManyField(Status)
+
+class Run(models.Model):
+    event_id = models.ForeignKey(Event, on_delete=models.CASCADE)
+    person_id = models.ForeignKey(Person, on_delete=models.PROTECT)
+    time = models.TimeField()
+    took_place = models.IntegerField()
+
+
